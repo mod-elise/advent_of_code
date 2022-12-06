f0d43a8a71b68f6a10d1fa784e8a711d12cea98f
mod-elise <73339513+mod-elise@users.noreply.github.com>
2022-12-06 10:19:38 +0000

    Day 6 functioned

--- 2022/day6_tuning.py
+++ 2022/day6_tuning.py
@@ -1,24 +1,4 @@
-def isStart(signal):
-    for letter in signal:
-        if signal.count(letter)>1:
-            return False
-        else:
-            continue
-    return True
-
-def scanSignals(signal_letters, target_length):
-    signal = ''
-    position = 0
-
-    for signal_letter in signal_letters:
-        signal = signal + signal_letter
-        position += 1
-        if len(signal)<target_length:
-            continue
-        if len(signal)>target_length:
-            signal = signal[1:]
-        if isStart(signal):
-            return ([signal, position])
+import aoc_functions
 
 
 with open('day6_input_file') as f:
@@ -26,8 +6,8 @@ with open('day6_input_file') as f:
 
 marker_target_length = 4
 message_target_length = 14
-markerSignal = scanSignals(signal_letters, marker_target_length)
-messageSignal = scanSignals(signal_letters, message_target_length)
+markerSignal = aoc_functions.scanSignals(signal_letters, marker_target_length)
+messageSignal = aoc_functions.scanSignals(signal_letters, message_target_length)
 
 print (markerSignal[0] + ' found at position ' +str(markerSignal[1]))
 print (messageSignal[0] + ' found at position ' +str(messageSignal[1]))
