d79bc612cc9d8b717e4e8de2370276fa93669e9a
mod-elise <73339513+mod-elise@users.noreply.github.com>
2022-12-13 17:16:36 +0000

    day 9 part 2

--- 2022/day9_knots.py
+++ 2022/day9_knots.py
@@ -1,85 +1,83 @@
-def moveHead(trail_history):
-    global i
+def moveHead():
     t_list = []
     move = movement.split()
     direction = move[0]
     magnitude = int(move[1])
-
     for r in range(0,magnitude,1):
-
+        #move the head       
         if direction == 'R':
-            H[horizontal] += 1
+            head[horizontal] += 1
         if direction == 'L':
-            H[horizontal] -= 1
+            head[horizontal] -= 1
         if direction == 'U':
-            H[vertical] += 1
+            head[vertical] += 1
         if direction == 'D':
-            H[vertical] -= 1
-        if not isTailTouching(H,T):
-            moveTail()
-            t_list.append(T.copy())
+            head[vertical] -= 1
+        #move all the tails
+        for i in range(0, len(knots), 1):
+            try:
+                if not isTailTouching(knots[i],knots[i+1]):
+                    knots[i+1] = moveTail(knots[i],knots[i+1])
+            except:
+                continue
+            #record the coords of the last tail 
+            t_list.append(knots[len(knots)-1].copy())
     return t_list
 
-def moveTail():
-    if (H[horizontal] == T[horizontal]): 
-        if H[vertical] > T[vertical]:
-            T[1] += 1
-            if not isTailTouching(H,T):
-                exit ('touching error')
+def moveTail(Front, Follower):
+    if (Front[horizontal] == Follower[horizontal]): 
+        if Front[vertical] > Follower[vertical]:
+            Follower[1] += 1
         else:
-            T[1] -= 1
-            if not isTailTouching(H,T):
-                exit ('touching error')            
-    elif (H[vertical] == T[vertical]):
-        if H[horizontal] > T[horizontal]:
-            T[horizontal] += 1
-            if not isTailTouching(H,T):
-                exit ('touching error')            
+            Follower[1] -= 1         
+    elif (Front[vertical] == Follower[vertical]):
+        if Front[horizontal] > Follower[horizontal]:
+            Follower[horizontal] += 1        
         else:
-            T[horizontal] -= 1
-            if not isTailTouching(H,T):
-                exit ('touching error')           
+            Follower[horizontal] -= 1          
     else:
-        if H[vertical] > T[vertical]: 
-            T[vertical] += 1
-        if H[vertical] < T[vertical]:
-            T[vertical] -= 1
-        if H[horizontal] > T[horizontal]:
-            T[horizontal] += 1
-        if H[horizontal] < T[horizontal]:
-            T[horizontal] -= 1
-    if not isTailTouching(H,T):
-        exit ('touching error')
-
+        if Front[vertical] > Follower[vertical]: 
+            Follower[vertical] += 1
+        if Front[vertical] < Follower[vertical]:
+            Follower[vertical] -= 1
+        if Front[horizontal] > Follower[horizontal]:
+            Follower[horizontal] += 1
+        if Front[horizontal] < Follower[horizontal]:
+            Follower[horizontal] -= 1
+    return Follower
 
 def isTailTouching(head_coord, tail_coord):
-
-
     if (abs(head_coord[0] - tail_coord[0]) < 2) and (abs(head_coord[1] - tail_coord[1]) < 2):
         return True
     return False
 
-# with open('day9_test') as f:
-#     movements = f.readlines()
-#     
-
 with open('day9_input_file') as f:
     movements = f.readlines()
 
-H = [0,0]
-T = [0,0]
-tail_history = [[0,0]]
+knots               = [
+                        [0,0],
+                        [0,0],
+                        [0,0],
+                        [0,0],
+                        [0,0],
+                        [0,0],
+                        [0,0],
+                        [0,0],
+                        [0,0],
+                        [0,0]
+                    ]
+head                = knots[0]
+tail_history        = [[0,0]]
 unique_tail_history = []
-
-horizontal  = 0
-vertical    = 1
+#constants
+horizontal          = 0
+vertical            = 1
 
 for movement in movements:
-   tail_history.extend(moveHead(tail_history))
-
+   tail_history.extend(moveHead())
 
 for tailloc in tail_history:
     if tailloc not in unique_tail_history:
         unique_tail_history.append(tailloc)
 
-print (f'The tail of one knot visits {len(unique_tail_history)} unique places')
+print (f'The tail visits {len(unique_tail_history)} unique places')
