7b4194cba19fe8e10eeb6240cbc31f47ee300ddd
mod-elise <73339513+mod-elise@users.noreply.github.com>
2023-12-10 17:28:45 +0000

    completes day 7 part 1

---
+++ 2023/day7/cards.py
@@ -0,0 +1,102 @@
+import math
+import time
+from collections import Counter
+
+
+
+def score_hand(hand):
+    score = 0
+    value = 0
+    for i, card in enumerate(hand):
+        if card == "A":
+            value = 14
+        elif card == "K":
+            value = 13
+        elif card == "Q":
+            value = 12
+        elif card == "J":
+            value = 11
+        elif card == "T":
+            value = 10
+        else:
+            value = int(card)
+        score+= math.pow(15,(5-(i+1))) * value
+    return score
+
+def categorise_hand (hand_obj):
+    # print (hand_obj["card_count"])
+    pair_count = 0
+    three_count = 0
+    four_count = 0
+    five_count = 0
+    for hand in hand_obj["card_count"]:
+        # print (hand, hand_obj["card_count"][hand])
+        if hand_obj["card_count"][hand] == 2:
+            pair_count += 1
+        if hand_obj["card_count"][hand] == 3:
+            three_count += 1
+        if hand_obj["card_count"][hand] == 4:
+            four_count += 1
+        if hand_obj["card_count"][hand] == 5:
+            five_count += 1
+    if pair_count == 1:
+        if three_count == 1:
+            hand_obj["category"] = "Full House"
+            hand_obj["score"] = (math.pow(15, 5) * 5) + score_hand(hand_obj["cards"])
+        else:
+            hand_obj["category"] = "One Pair"
+            hand_obj["score"] = (math.pow(15, 5) * 2) + score_hand(hand_obj["cards"])
+    elif pair_count == 2:
+        hand_obj["category"] = "Two Pairs"
+        # print (f'{hand_obj["cards"]}, ')
+        hand_obj["score"] = (math.pow(15, 5) * 3) + score_hand(hand_obj["cards"])
+    elif three_count == 1:
+        hand_obj["category"] = "Three of a Kind"
+        hand_obj["score"] = (math.pow(15, 5) * 4)+ score_hand(hand_obj["cards"])
+    elif four_count == 1:
+        hand_obj["category"] = "Four of a Kind"
+        hand_obj["score"] = (math.pow(15, 5) * 6) + score_hand(hand_obj["cards"])
+    elif five_count == 1:
+        hand_obj["category"] = "Five of a Kind"
+        hand_obj["score"] = (math.pow(15, 5) * 7) + score_hand(hand_obj["cards"])
+    else:
+        hand_obj["category"] = "High Card"
+        hand_obj["score"] = math.pow(15, 5) + score_hand(hand_obj["cards"])
+
+    return hand_obj
+
+
+
+
+start_time = time.time()
+f = open("input.txt", "r")
+hands = f.readlines()
+hand_objs = []
+running_score = 0
+
+for hand_details in hands:
+    hand_details = hand_details.split()
+
+    card_count = Counter(hand_details[0])
+    hand_obj = {
+        "raw" : hand_details,
+        "cards" : hand_details[0],
+        "bid" : int(hand_details[1]),
+        "card_count" : card_count
+    }
+    hand_objs.append(hand_obj)
+
+for i, hand_obj in enumerate(hand_objs):
+    hand_objs[i] = categorise_hand(hand_obj)
+
+
+sorted_hand_objs = sorted(hand_objs, key=lambda x: x["score"], reverse=False)
+
+
+
+
+for i in range(0, len(sorted_hand_objs)):
+    print (sorted_hand_objs[i]["category"], sorted_hand_objs[i]["score"], sorted_hand_objs[i]["cards"], (i+1) * sorted_hand_objs[i]["bid"])
+    running_score += (i+1) * sorted_hand_objs[i]["bid"]
+
+print (running_score)
