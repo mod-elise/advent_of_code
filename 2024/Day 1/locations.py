f0e655b07515f34ef88feb503504169c149b2bf2
mod-elise <73339513+mod-elise@users.noreply.github.com>
2024-12-01 11:41:55 +0000

    Day 1 part 2

--- 2024/Day 1/locations.py
+++ 2024/Day 1/locations.py
@@ -3,4 +3,10 @@ with open('input.txt') as f:
     contents = [tuple(map(int, x.split())) for x in contents]
 
 left, right = (sorted(x) for x in zip(*contents))
-print (sum([abs(x-y) for x,y in zip(left, right)]))
+print (f'The difference score is ' + str(sum([abs(x-y) for x,y in zip(left, right)])))
+
+similarity_score = 0
+for x,y in zip(left, right):
+    similarity_score += x * right.count(x)
+
+print (f'The similarity score is ' + str(similarity_score))
