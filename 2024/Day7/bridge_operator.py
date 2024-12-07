46d864751180639ad0acc2900b8cf0467e551ab1
mod-elise <73339513+mod-elise@users.noreply.github.com>
2024-12-07 13:23:34 +0000

    Used AI to help make it more efficient.  Not sure I learned anything from this

--- 2024/Day7/bridge_operator.py
+++ 2024/Day7/bridge_operator.py
@@ -5,28 +5,34 @@ with open('input.txt') as f:
 
 def perform_operation (operator, num1, num2):
     if operator == '+':
-        return int(num1) + int(num2)
-    elif operator == '*':
-        return int(num1) * int(num2)
-    elif operator == 'C':
         return num1 + num2
+    elif operator == '*':
+        return num1 * num2
+    elif operator == 'C':
+        return int(str(num1) + str(num2))
 
 def is_Valid (value,  numbers, operator_combos):
-
     for operator_combo in operator_combos:
-        left = (numbers[0])
-        for idx, operator in enumerate(operator_combo):
-            left = str(perform_operation(operator, left, (numbers[idx+1])))
-        if int(left) == value:
-            return True
+        left = numbers[0]
+        for operator, num in zip(operator_combo, numbers[1:]):
+            left = perform_operation(operator, left, num)
+            if left > value:
+                break
+            else:
+                if left == value:
+                  return True
     return False
 
-def calibrate (callibration_list, operators):
+
+def calibrate(callibration_list, operators):
     total = 0
+    operator_combos = {i: list(itertools.product(operators, repeat=i)) for i in range(11)}
+
     for callibration in callibration_list:
-        total_operators = len(callibration['numbers']) -1
-        combinations = list(itertools.product(operators, repeat=total_operators))
-        if is_Valid(callibration['value'], callibration['numbers'], combinations):
+        total_operators = len(callibration['numbers']) - 1
+        sorted_combinations = operator_combos.get(total_operators,
+                                                  list(itertools.product(operators, repeat=total_operators)))
+        if is_Valid(callibration['value'], callibration['numbers'], sorted_combinations):
             total += callibration['value']
     return total
 
@@ -35,7 +41,7 @@ for line in lines:
     line = line.split(':')
     callibration_data = {
         'value': int(line[0]),
-        'numbers': line[1].split()
+        'numbers': [int(x) for x in line[1].split()]
     }
     callibration_list.append(callibration_data)
 
@@ -43,10 +49,10 @@ for line in lines:
 start_time = time.time()
 operators = ['+', '*']
 print (f'Part 1: {calibrate(callibration_list, operators)}')
-print ("--- %s seconds ---" % (time.time() - start_time)) # 2 seconds on my machine
+print ("--- %s seconds ---" % (time.time() - start_time)) # 0.5 seconds on my machine
 
 start_time = time.time()
 operators = ['+', '*', 'C']
 total = 0
 print (f'Part 2: {calibrate(callibration_list, operators)}')
-print ("--- %s seconds ---" % (time.time() - start_time)) # 75 seconds on my machine
+print ("--- %s seconds ---" % (time.time() - start_time)) # 45 seconds on my machine
