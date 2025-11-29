3415d8e72e4af2f27d2f5108bf5b7d472ebafd4b
mod-elise <mod@kijko.com>
2025-11-29 18:06:47 +0000

    Adding noun,verb functionality

--- 2019/day2/intcode.py
+++ 2019/day2/intcode.py
@@ -2,12 +2,11 @@ with open("input.txt") as f:
     int_array = f.read()
 int_array = [int(x) for x in int_array.split(',')]
 step=4
-int_array[1] = 12
-int_array[2] = 2
 
-
-def function1 (input_array):
+def main (input_array, noun, verb):
     for i in range(0, len(input_array), step):
+        input_array[1] = noun
+        input_array[2] = verb
         if input_array[i] == 99:
             break
         opcode = input_array[i]
@@ -20,6 +19,14 @@ def function1 (input_array):
             input_array[target] = input_array[param1] * input_array[param2]
         else:
             print(f"Unknown opcode {opcode} at position {i}")
-    return int_array[0]
+    return input_array[0]
 
-print(function1(int_array))
+#part 1
+print(f'Part 1 answer: {main(int_array.copy(),12,2)}')
+
+#part 2
+for noun in range(100):
+    for verb in range(100):
+        if main(int_array.copy(),noun,verb) == 19690720:
+            print(f'Part 2 answer: {100 * noun + verb}')
+            exit()
