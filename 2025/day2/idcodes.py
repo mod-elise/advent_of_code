09b21e58533edba6cdc8322069ef11782cf4bc76
mod-elise <mod@kijko.com>
2025-12-02 13:46:36 +0000

    Part 2 of Day 2

--- 2025/day2/idcodes.py
+++ 2025/day2/idcodes.py
@@ -5,6 +5,17 @@ with open("input.txt", "r") as file:
 def is_even_length(id_code):
     return len(id_code) % 2 == 0
 
+def isInvalidCode(id_code, code_length):
+    for i in range(1, code_length):
+        chunks = []
+        if code_length%i== 0:
+            for j in range(0,code_length,i):
+                chunks.append(id_code[j:j+i])
+        chunks_set = set(chunks)
+        if len(chunks_set) == 1:
+            return True
+    return False
+
 invalid_id_codes_pt1 = []
 invalid_id_codes_pt2 = []
 
@@ -12,6 +23,7 @@ for id_range in id_range_list:
     start, end = map(int, id_range.split("-"))
     for id_code in range(start, end + 1):
         id_code_str = str(id_code)
+        code_length = len(id_code_str)
 
         # part 1
         if is_even_length(id_code_str):
@@ -21,9 +33,8 @@ for id_range in id_range_list:
                 invalid_id_codes_pt1.append(id_code_str)
 
         # part 2
-        for i in range(len(id_code_str) - 1):
-            continue
-
-int_invalid_id_codes = [int(x) for x in invalid_id_codes_pt1]
-
-print(f'Sum of invalid ID codes: {sum(int_invalid_id_codes)}')
+        if isInvalidCode(id_code_str, code_length):
+            invalid_id_codes_pt2.append(id_code_str)
+ 
+print(f'Sum of invalid ID codes: {sum([int(x) for x in invalid_id_codes_pt1])}')
+print(f'Sum of invalid ID codes for part 2: {sum([int(x) for x in invalid_id_codes_pt2])}')
