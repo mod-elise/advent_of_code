e1e6c3232ac4c98e41f2e96e0dfe28d665dbe5c5
mod-elise <mod@kijko.com>
2025-12-03 16:02:13 +0000

    Day3 2025 part2

--- 2025/day3/batteries.py
+++ 2025/day3/batteries.py
@@ -1,25 +1,32 @@
-with open('input.txt', 'r') as file:
-    banks = file.read().splitlines()
+try:
+    with open('input.txt', 'r') as file:
+        print ("found file")
+        banks = file.read().splitlines()
+except FileNotFoundError:
+        print ("file not found, using hardcoded ranges")
+        banks = ["987654321111111", "811111111111119", "234234234234278", "818181911112111"]
 
-def getHighestBattery(bank):
+def getHighestBattery(bank, largest_pos, batteries_left):
     largest = 0
-    second_largest= 0
     i=0
-    for number in bank:
-        if int(number) > int(largest) and i != len(bank)-1:
-            largest = number
+    for i in range (largest_pos, len(bank)-batteries_left,1):
+        if int(bank[i]) > int(largest):
+            largest = bank[i]
             largest_pos = i
         i+=1
+    return largest, largest_pos
 
-    for i in range (largest_pos+1, len(bank),1):
-        if int(bank[i]) >= int(second_largest):
-            second_largest = bank[i]
+number_of_batteries = [2,12] 
 
-    return int(largest + second_largest)
+for battery_quantity in number_of_batteries:
+    joltage_list = []
+    for bank in banks:
+        joltage = ''
+        last_pos = -1
+        for j in range (1, battery_quantity+1, 1):
+            batteries_left = battery_quantity - j + 1
+            battery, last_pos = getHighestBattery(bank, last_pos+1, batteries_left-1)
+            joltage = joltage + battery
+        joltage_list.append(int(joltage))
 
-sum_of_highest = 0
-
-for bank in banks:
-    sum_of_highest += getHighestBattery(bank)   
-
-print (sum_of_highest)
+    print (f'sum of all joltages for {battery_quantity} batteries: {sum(joltage_list)}')
